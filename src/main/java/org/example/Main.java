88b473c7707a4bb951a9f444664568780149a3e0
Bruno <brunoesquiva12111@gmail.com>
2023-01-13 20:16:34 +0100

    Sopa De letras Funcional sin limites

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import java.util.Scanner;
 
 public class Main {
@@ -5,51 +7,41 @@ public class Main {
         String temporal = args[0];
 
         int n1 = Integer.parseInt(temporal);
-        int palabramaslarga = 0;
+        int palabramaslarga=0;
+//nada
         int temporal2 = Integer.parseInt(temporal), temporal3;
-        String[] palabra = new String[temporal2*2];
+        String[] palabra = new String[temporal2];
         Scanner input = new Scanner(System.in);
         char[] letras = new char[temporal2];
-
-
-
-
-
         //pedimos las palabras que queremos poner en la sopa de letras
         for (int i = 0; i < temporal2; i++) {
             System.out.println("Dime una palabra," + " quedan " + n1 + " palabras por poner");
             palabra[i] = input.nextLine();
-            palabramaslarga = palabra[0].length();
-            if (palabramaslarga < palabra[i].length()) {
-                palabramaslarga = palabra[i].length();
+            palabramaslarga= palabra[0].length();
+            if(palabramaslarga<palabra[i].length()){
+                palabramaslarga= palabra[i].length();
             }
             n1--;
 
+
         }
-        letras = palabra[0].toCharArray();
-        System.out.println(letras);
+        String [][] sopa = new String[3*palabramaslarga][3*palabramaslarga];
 
-        int p = 3 * palabramaslarga;
-        String[][] sopa = new String[p][p];
+        int palabrarandom1 = 3;
+        System.out.println(palabrarandom1);
+        int palabrarandom2 = 4;
 
-        int palabrarandom1 = 0;
-        int palabrarandom2 = (int) (Math.random() * p);
+
 
         //sopa creada con Letra metida
-        for (int i = 0; i < sopa.length; i++) {
+        for (int i=0; i< sopa.length; i++){
             System.out.print("\n");
-            for (int j = 0; j < sopa[i].length; j++) {
-                //creamos letrasaleatorias para rellenar la sopa de letras
-                char letraaleatoria = (char) (Math.random() * 25 + 65);
-                if (i == palabrarandom1 && j == palabrarandom2) {
-                    System.out.print(letra[i]);
-                    {
-
-                    }
-                } else {
-                    System.out.print(letraaleatoria);
+            for (int j=0; j< sopa[i].length; j++){
+                if (i==palabrarandom1 && j==palabrarandom2){
+                    System.out.print(palabra[0]);
+                } else{
+                    System.out.print(".");
                 }
-
             }
 
         }
